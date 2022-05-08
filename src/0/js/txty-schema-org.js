13e732f7faf0d57f3da6b84249ecb77ad4a70cd4
ytyaru <yttry0@gmail.com>
2022-05-08 16:25:28 +0900

    TestTxtySchemaOrgArticleテスト開始

--- src/0/js/txty-schema-org.js
+++ src/0/js/txty-schema-org.js
@@ -15,21 +15,32 @@ class TxtySchemaOrgPerson extends TxtySchemaOrgGenerator {
         if (url) { result.url = url; }
         return result
     }
+    generateFromTxty(txtyObj) {
+        const result = this.generateTypeObj('Person')
+        result.name = txtyObj.name
+        if (0 < txtyObj.options.length) {
+            result.url = txtyObj.options[0];
+            if (1 < txtyObj.options.length) {
+                result.sameAs = txtyObj.options.slice(1);
+            }
+        }
+        return result
+    }
 }
 class TxtySchemaOrgArticle extends TxtySchemaOrgGenerator {
     generate(txt, indent=null) {
-        const block = Txty.lines(txt)
+        const block = Txty.lines(txt)[0]
         if (0 === block.length) { throw new TxtySchemaOrgArticleError(`引数txtが空です。有効なテキストを入力してください。`); }
-        const result = this.generateContextTypeObj('Article'); // Article, NewsArticle, BlogPosting
+        let result = this.generateContextTypeObj('Article'); // Article, NewsArticle, BlogPosting
         result.headline = block[0].name
         if (0 < block[0].options.length) { result.image = block[0].options; } // 画像URL配列（16x9,4x3,1x1）
         if (1 < block.length) {
-            authorBlock = block[1]
+            let authorBlock = block[1]
             if (2 < block.length) {
-                result.assign(this.#setDates(block[1]))
+                result = Object.assign(result, this.#setDates(block[1]))
                 authorBlock = block[2]
             }
-            result.author = new TxtySchemaOrgPerson().generate(authorBlock.name, (0 < authorBlock.options.length) ? authorBlock.options[0] : null)
+            result.author = new TxtySchemaOrgPerson().generateFromTxty(authorBlock)
         }
         return result
     }
